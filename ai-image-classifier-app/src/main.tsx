7f734b9cffdd8bd4d1b4033439a902284f93304e
Aaron J. Goldstein <97154040+Windz-GameDev@users.noreply.github.com>
2024-04-25 22:40:13 -0400

    Added bootswatch

--- ai-image-classifier-app/src/main.tsx
+++ ai-image-classifier-app/src/main.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/css/bootstrap.css";
+
+// Use bootswatch darkly
+import "../bootswatch/bootstrap_darkly.min.css";
+
+// Use bootswatch lux
+// import "../bootswatch/bootstrap_lux.min.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
